21dc9ee32701fc8f7d26705dd42d6a615d058892
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-22 17:31:25 -0300

    Adicionando o CSS modules no projeto

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Button from '../Button';
-import './style.scss';
+import style from './Form.module.scss';
 
 class Form extends React.Component {
     render() {
         return (
-            <form className="novaTarefa">
-                <div className="inputContainer">
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
@@ -19,7 +19,7 @@ class Form extends React.Component {
                     />
                 </div>
 
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import List from '../components/List';
 import Form from '../components/Form';
-import './style.scss';
+import style from './App.module.scss';
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Form />
       <List />
     </div>
