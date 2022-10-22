7d6347ed36457137b7952fbf86e87788bb76501a
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-22 18:22:18 -0300

    Aplicação de props em list e criação de Item

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
@@ -17,10 +18,10 @@ function List() {
             <h2> Estudo do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3> {item.tarefa} </h3>
-                        <span> {item.tempo} </span>
-                    </li>
+                    <Item
+                        key={index}
+                        {...item}
+                    />
                 ))}
             </ul>
         </aside>
