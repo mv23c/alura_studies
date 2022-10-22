c04dcef4a8db6ea661d8e8add422c368aef3de1b
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-22 16:11:53 -0300

    Adição de estilo e algumas pequenas alterações

---
+++ alura-studies/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from '../components/List';
+import Form from '../components/Form';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
